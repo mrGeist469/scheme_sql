f1d078c2b636cf285f34b79c8685b72024b9223d
mrGeist469 <ya.ivashka20@yandex.ru>
2021-06-04 11:09:40 +0300

    correcting code

--- scheme.sql
+++ scheme.sql
@@ -13,12 +13,12 @@ create table if not exists Albums (
     id serial primary key,
     executors_id integer references Executors (id),
     albums_name varchar (80) not null
-    release_year date not null
+    release_year integer not null
 );
 
 create table if not exists Track_names (
     id serial primary key,
     albums_id integer references Albums (id),
     track_name varchar (80) not null
-    track_duration time not null
+    track_duration integer not null
 );
